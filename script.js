4fcd26e2617891da20b7788af73b3d37d39243e2
Deeshma Deepak <deeshmadas@gmail.com>
2024-09-10 22:36:08 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const img=document.getElementById("userImg")
+const name=document.getElementById("name")
+const email=document.getElementById("email")
+const fetchUser=()=>{
+    fetch('https://randomuser.me/api/')
+    .then(Response=>Response.json())
+    //.then(data=>console.log(data))
+    .then(data=>{
+        const user=data.results[0];
+        name.textContent=`${user.name.first} ${user.name.last}`
+        email.textContent=user.email
+        img.src=user.picture.large
+    })
+    .catch(error=>console.log(error));
+    
+}
